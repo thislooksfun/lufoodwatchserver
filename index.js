58047b771d82c0b49a428c9cf870f1c60508a1dd
thislooksfun <thislooksfun@repbot.org>
2018-09-01 14:45:49 -0500

    Clean up database on quit

--- index.js
+++ index.js
@@ -3,9 +3,29 @@
 global.pquire = require("pquire");
 global.log = require("tlf-log");
 
+const nodeCleanup = require("node-cleanup");
+
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").load();
 }
+
+var db;
+nodeCleanup(function(exitCode, signal) {
+  console.log();
+  log.trace("Cleaning up...");
+  db = db || pquire("lib/api/v1/database");
+  if (db.isOpen) {
+    db.close().then(function() {
+      log.trace("Cleanup finished successfully, exiting!");
+      process.kill(process.pid, signal);
+    });
+    
+    // Don't call handler again
+    nodeCleanup.uninstall();
+    return false;
+  }
+});
+
 
 (async function() {
   try {
