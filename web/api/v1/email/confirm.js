a21d904b57e93cb3317e5b3d01f33b36f7ce5a75
thislooksfun <thislooksfun@repbot.org>
2018-09-01 14:45:37 -0500

    Seperate 'user' specific methods out of database.js

--- web/api/v1/email/confirm.js
+++ web/api/v1/email/confirm.js
@@ -2,14 +2,12 @@
 
 const wrap = pquire("lib/util/wrap");
 const codes = pquire("lib/util/httpCodes");
-const db = pquire("lib/api/v1/database");
-// const email = pquire("lib/api/v1/notify/email");
-
+const users = pquire("lib/api/v1/db/users");
 
 
 async function get(req, res) {
   console.log(req.params.token);
-  await db.confirmEmail(req.params.token);
+  await users.confirmEmail(req.params.token);
   
   // TODO: Make this page look better
   res.status(codes.ok).send("Success!");
--- web/api/v1/signup.js
+++ web/api/v1/signup.js
@@ -2,17 +2,16 @@
 
 const wrap = pquire("lib/util/wrap");
 const codes = pquire("lib/util/httpCodes");
-const db = pquire("lib/api/v1/database");
-// const email = pquire("lib/api/v1/notify/email");
+const users = pquire("lib/api/v1/db/users");
 
-const { MissingParameterError, InvalidParameterTypeError } = pquire("lib/util/errors");
-const DuplicateUserError = db.DuplicateUserError;
+const { MissingParameterError, InvalidParameterTypeError } = pquire("lib/util/verifyParam");
+const DuplicateUserError = users.DuplicateUserError;
 
 
 
 async function post(req, res) {
   try {
-    await db.signUp(req.body.email, req.body.password);
+    await users.signUp(req.body.email, req.body.password);
     res.status(codes.ok).send("Success!");
   } catch (e) {
     if (e instanceof MissingParameterError || e instanceof InvalidParameterTypeError || e instanceof DuplicateUserError) {
@@ -21,12 +20,6 @@ async function post(req, res) {
       throw e;
     }
   }
-  
-  
-  // await email.send({to: req.body.email, subject: "Hello there!", text: "It works! Huzzah!"});
-  
-  
-  // res.json({hello: (await password.hash(req.body.email)).length});
 }
 
 module.exports = {
