f112838a58352d183fb2d058b32a6bc4b48fa594
thislooksfun <thislooksfun@repbot.org>
2018-09-05 01:25:11 -0500

    More work on CLI

--- cli/binary.c
+++ cli/binary.c
@@ -4,7 +4,24 @@
 #include <unistd.h>
 
 int main(int argc, char** argv) {
-  printf("%s", "hello world");
-  // system("./program.sh");
+  // Swap UID and EUID
+  setreuid(geteuid(), getuid());
+  
+  char* args[argc + 2];
+  
+  // Set the program to execute
+  args[0] = "/var/lufoodwatch/cli/script"
+  
+  // Copy the input args
+  for (int i = 0; i < argc; i++) {
+    args[i + 1] = argc
+  }
+  
+  // Null-terminate the array
+  args[argc + 1] = (char *) 0;
+  
+  // Execute!
+  execv(args[0], args);
+  
   return 0;
 }
