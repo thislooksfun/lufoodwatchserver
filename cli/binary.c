c14b9f9504771b8b9210f0bd8c9a144175137ccb
thislooksfun <thislooksfun@repbot.org>
2018-09-05 00:44:25 -0500

    Use binary instead of script
    This is required because the 'setuid' bit only works on binary executables, not on shell scripts for some reason.

---
+++ cli/binary.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(int argc, char** argv) {
+  printf("%s", "hello world");
+  // system("./program.sh");
+  return 0;
+}
