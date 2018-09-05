4b2b02609fed011b8be390ea09c13a2c1c16b1fc
thislooksfun <thislooksfun@repbot.org>
2018-09-05 01:56:20 -0500

    Fix and finish binary

--- cli/binary.c
+++ cli/binary.c
@@ -4,24 +4,25 @@
 #include <unistd.h>
 
 int main(int argc, char** argv) {
-  // Swap UID and EUID
-  setreuid(geteuid(), getuid());
+  // Set UID to EUID (no, 'setuid' doesn't work)
+  setreuid(geteuid(), -1);
   
-  char* args[argc + 2];
+  char* args[argc + 1];
   
   // Set the program to execute
   args[0] = "/var/lufoodwatch/cli/script";
   
-  // Copy the input args
-  for (int i = 0; i < argc; i++) {
-    args[i + 1] = argc;
+  // Copy the input args (start at 1 because argv[0] is always the path to this executable)
+  for (int i = 1; i < argc; i++) {
+    args[i] = argv[i];
   }
   
   // Null-terminate the array
-  args[argc + 1] = (char *) 0;
+  args[argc] = (char *) 0;
   
   // Execute!
   execv(args[0], args);
+  perror("execv");
   
   return 0;
 }
